84f7b1e0774f4556021a5c39adefc6252dcdcd0c
J.D.Jayatilake <84485753+dillonjayatilake@users.noreply.github.com>
2026-02-19 08:27:36 +0530

    Full backend sample done

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userRoutes = require("./Routes/Tharusha/UserRoutes");
+const vaccineRoutes  = require('./Routes/Dillon/VaccineRoutes');
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 // Routes
 app.use("/users", userRoutes);
+app.use('/api/vaccines', vaccineRoutes);
 
 module.exports = app;
